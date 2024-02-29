0364212c0850c9646dfb39741a6b9d22037360b5
kevinoo <lucichkevin@gmail.com>
2024-02-29 10:17:34 +0100

    Add PHP7.2 version

--- _deploy.sh
+++ _deploy.sh
@@ -16,6 +16,12 @@ if [ -z "$1" ] || [ "$1" == '--help' ] || [ "$1" == '-h' ]; then
 fi
 
 TAG=""
+if [ "$1" == 'php72' ]; then
+  TAG="php7.2-apache"
+fi
+if [ "$1" == 'php72-locales' ]; then
+  TAG="php7.2-apache-locales"
+fi
 if [ "$1" == 'php81' ]; then
   TAG="php8.1-apache"
 fi
--- _deploy_all.sh
+++ _deploy_all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source ./.env
 
+./_deploy.sh php72
+./_deploy.sh php72-locales
 ./_deploy.sh php81
 ./_deploy.sh php81-locales
 ./_deploy.sh php82
