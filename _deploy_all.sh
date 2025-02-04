2854ce18554bc4379dd4975e47ed538dee76b784
kevinoo <lucichkevin@gmail.com>
2025-02-04 09:33:40 +0100

    Add PHP v8.4

--- _deploy_all.sh
+++ _deploy_all.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 source ./.env
 
-./_deploy.sh php7.2-apache
-./_deploy.sh php7.2-apache-locales
-
 ./_deploy.sh php8.1-apache
 ./_deploy.sh php8.1-apache-locales
 
@@ -14,3 +11,7 @@ source ./.env
 ./_deploy.sh php8.3-apache
 ./_deploy.sh php8.3-apache-locales
 ./_deploy.sh php8.3-fpm-alpine
+
+./_deploy.sh php8.4-apache
+./_deploy.sh php8.4-apache-locales
+./_deploy.sh php8.4-fpm-alpine
